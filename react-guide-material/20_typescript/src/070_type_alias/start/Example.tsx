ddf1c62bc205bf9476933779a68e83f5a4f52414
kawahara kazuma <kawahara.kazuma0525@gmail.com>
2024-02-25 17:14:51 +0900

    typescript実装

--- react-guide-material/20_typescript/src/070_type_alias/start/Example.tsx
+++ react-guide-material/20_typescript/src/070_type_alias/start/Example.tsx
@@ -1,5 +1,28 @@
+import {type} from "os";
+
 const Example = () => {
-  // 練習用
+  type Usre = {
+      name: string,
+      age: number
+  }
+
+  const user: Usre = {name: "Taro", age: 10}
+
+    type UserName = string;
+    type UserAge = number;
+    type UserGender = "man" | "woman" | "others";
+
+    type UserProfile = {
+        name: UserName,
+        age: UserAge,
+        gender: UserGender,
+    }
+
+    const userProfile: UserProfile = {
+        name : "kazuma",
+        age: 27,
+        gender: "man"
+    }
 };
 
 export default Example;
--- react-guide-material/20_typescript/src/080_function_type/start/Example.tsx
+++ react-guide-material/20_typescript/src/080_function_type/start/Example.tsx
@@ -1,5 +1,15 @@
 const Example = () => {
-  // 練習用
+    function sum1(x: number, y:number) {
+        return x + y;
+    }
+
+    const result1 = sum1(1,2);
+
+    const sum2 = (x: number, y:number):number =>  x + y;
+
+    type Sum = (x: number,y :number) => number;
+    const sum3: Sum = (x,y) => x+y;
+    const result = sum3(1,2);
 };
 
 export default Example;
